9c8c329eb3014ab54fee8fe8aad04f0e3140de04
Bartosz Jarmuz <bartosz.jarmuz@gmail.com>
2019-05-21 16:42:38 +0200

    show mutability

--- src/1. Collections/Collections/CollectionsTests/CorruptingList_MutableObject.cs
+++ src/1. Collections/Collections/CollectionsTests/CorruptingList_MutableObject.cs
@@ -29,39 +29,92 @@ namespace CollectionsTests
                 this.extensions.AddRange(extensions);
             }
 
-            public IReadOnlyCollection<Extension> Extensions_IReadOnlyCollection_RealImplementation => extensions.AsReadOnly();
+            public List<Extension> Extensions_List => extensions;
+            public IEnumerable<Extension> Extensions_IEnumerable => this.extensions;
             public IEnumerable<Extension> Extensions_IEnumerable_Copy => new List<Extension>(extensions);
+            public IReadOnlyCollection<Extension> Extensions_IReadOnlyCollection_RealImplementation => extensions.AsReadOnly();
 
+            public ImmutableList<Extension> Extensions_ImmutableList => ImmutableList.CreateRange<Extension>(this.extensions);
+            public IReadOnlyCollection<Extension> Extensions_ImmutableIReadOnlyCollection => ImmutableList.CreateRange<Extension>(this.extensions);
         }
 
         [TestMethod]
-        public void ChangeListItems_IEnumerable_CorruptionFailed()
+        public void ChangeListItems_DereferencingIsNotAnOption_CorruptionFailed()
         {
             FileType picture = new FileType(new Extension(".bmp"), new Extension(".jpg"));
 
-            Extension bmp = picture.Extensions_IEnumerable_Copy.First(x => x.Value == ".bmp");
-            //we get the reference to one extensions and change it...
-            bmp.Value = ".exe";
+            //we can get the bmp object from the list... 
+            Extension bmp = picture.Extensions_List.FirstOrDefault(x => x.Value == ".bmp");
+            //but changing the value will only dereference the current variable, *not* the actual referenced object
+            bmp = new Extension(".exe");
 
-            //...and it's changed in the object, we have an invalid state
-            Assert.AreEqual(1, picture.Extensions_IEnumerable_Copy.Count(x=>x.Value == ".exe"));
-            Assert.AreEqual(0, picture.Extensions_IEnumerable_Copy.Count(x=>x.Value == ".bmp"));
-            
+            //...so we still have a valid state
+            Assert.AreEqual(0, picture.Extensions_List.Count(x => x.Value == ".exe"));
+            Assert.AreEqual(1, picture.Extensions_List.Count(x => x.Value == ".bmp"));
         }
 
         [TestMethod]
-        public void ChangeListItems_IReadOnlyCollection_CorruptionFailed()
+        public void ChangeListItems_IEnumerable_Dereference_CorruptionSuccess()
         {
             FileType picture = new FileType(new Extension(".bmp"), new Extension(".jpg"));
 
-            Extension bmp = picture.Extensions_IReadOnlyCollection_RealImplementation.First(x => x.Value == ".bmp");
-            //we get the reference to one extensions and change it...
-            //the 'Read only' does not protect from it
-            bmp.Value = ".exe";
+            //we can get the bmp object from the list... 
+            Extension bmp = picture.Extensions_IEnumerable.FirstOrDefault(x => x.Value == ".bmp");
+            int index = ((List<Extension>)picture.Extensions_IEnumerable).IndexOf(bmp);
+            //and dereference the item at that index
+            ((List<Extension>)picture.Extensions_IEnumerable)[index] = new Extension(".exe");
 
             //...and it's changed in the object, we have an invalid state
-            Assert.AreEqual(1, picture.Extensions_IReadOnlyCollection_RealImplementation.Count(x => x.Value == ".exe"));
-            Assert.AreEqual(0, picture.Extensions_IReadOnlyCollection_RealImplementation.Count(x => x.Value == ".bmp"));
+            Assert.AreEqual(1, picture.Extensions_IEnumerable.Count(x => x.Value == ".exe"));
+            Assert.AreEqual(0, picture.Extensions_IEnumerable.Count(x => x.Value == ".bmp"));
+        }
+
+        [TestMethod]
+        public void ChangeListItems_IEnumerableCopy_Dereference_CorruptionFailed()
+        {
+            FileType picture = new FileType(new Extension(".bmp"), new Extension(".jpg"));
+
+            //we can get the bmp object from the list... 
+            Extension bmp = picture.Extensions_IEnumerable_Copy.FirstOrDefault(x => x.Value == ".bmp");
+            int index = ((List<Extension>) picture.Extensions_IEnumerable_Copy).IndexOf(bmp);
+            //and dereference the item at that index
+            ((List<Extension>)picture.Extensions_IEnumerable_Copy)[index] = new Extension(".exe");
+
+            //...but the list is a copy, thus we're dereferencing the item on the copy...
+            Assert.AreEqual(0, picture.Extensions_IEnumerable_Copy.Count(x => x.Value == ".exe"));
+            Assert.AreEqual(1, picture.Extensions_IEnumerable_Copy.Count(x => x.Value == ".bmp"));
+        }
+        
+        [TestMethod]
+        public void ChangeListItems_IEnumerableCopy_ChangeObjectValue_CorruptionSucceeds()
+        {
+            FileType picture = new FileType(new Extension(".bmp"), new Extension(".jpg"));
+
+            //we can get the bmp object from the list... 
+            Extension bmp = picture.Extensions_IEnumerable_Copy.FirstOrDefault(x => x.Value == ".bmp");
+            //and since it's mutable, change its property
+            bmp.Value = ".exe";
+
+            //...and regardless whether list is a copy, the object has changed, and the list itself is broken...
+            Assert.AreEqual(1, picture.Extensions_IEnumerable_Copy.Count(x => x.Value == ".exe"));
+            Assert.AreEqual(0, picture.Extensions_IEnumerable_Copy.Count(x => x.Value == ".bmp"));
+        }
+
+        [TestMethod]
+        public void ChangeListItems_ImmutableList_CorruptionFailed()
+        {
+            FileType picture = new FileType(new Extension(".bmp"), new Extension(".jpg"));
+            //immutable list allows you to make changes...
+            picture.Extensions_ImmutableList.RemoveAll(x => x.Value == ".bmp");
+            picture.Extensions_ImmutableList.Add(new Extension(".exe"));
+
+            //...but the changes only happen on the new copy of the object, whereas the real one is still there
+            Assert.AreEqual(0, picture.Extensions_ImmutableList.Count(x=>x.Value == ".exe"));
+            Assert.AreEqual(1, picture.Extensions_ImmutableList.Count(x=>x.Value == ".bmp"));
+
+            //however, much like with the string, operations on this object (which are void on 'regular' Lists) now return the changed value
+            var modifiedList = picture.Extensions_ImmutableList.Add(new Extension(".exe"));
+            Assert.AreEqual(1, modifiedList.Count(x => x.Value == ".exe"));
         }
 
     }
